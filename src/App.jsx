ede481688d8e793e610f04975202ea1f515eef70
Erick Vicentin <erickvicentin14@hotmail.com>
2026-04-18 20:55:41 -0300

    feat: add new pages for login, registration, employment request, news, and wellness; update routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Registration />} />
+      <Route path="/employment-request" element={<EmploymentRequest />} />
+      <Route path="/news" element={<News />} />
+      <Route path="/wellness" element={<Wellness />} />
+      <Route path="/levels" element={<Levels />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
